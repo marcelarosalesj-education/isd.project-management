fd727d84a87e5094e602b3abe67011c5df95767b
Marcela Rosales <marcelarosalesj@gmail.com>
2016-11-13 18:47:39 -0600

    Added AG
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- EngineerigEconomics/eeEquivalence.py
+++ EngineerigEconomics/eeEquivalence.py
@@ -16,9 +16,11 @@ def AP(i, n):
 def PG(i, n):
 	return (pow(1+i,n) - i*n - 1)/(pow(i,2)*pow(1+i,n))
 
-a = [250000,275000,302500, 332750, 366025]
-ii = 0.15
+def AG(i, n):
+	return (pow(1+i,n) - i*n - 1)/( i*(pow(1+i,n)-1) )
 
-res = a[0]*PA(ii,5) + a[1]*PA(ii,5)*PF(ii,5) + a[2]*PA(ii,5)*PF(ii,10) + a[3]*PA(ii,5)*PF(ii,15) + a[4]*PA(ii,5)*PF(ii,20) - 65000*PA(ii, 25) - 6000*PG(ii,25) + 200000*PF(ii, 25)
 
-print res 
+#a = [250000,275000,302500, 332750, 366025]
+#ii = 0.15
+#res = a[0]*PA(ii,5) + a[1]*PA(ii,5)*PF(ii,5) + a[2]*PA(ii,5)*PF(ii,10) + a[3]*PA(ii,5)*PF(ii,15) + a[4]*PA(ii,5)*PF(ii,20) - 65000*PA(ii, 25) - 6000*PG(ii,25) + 200000*PF(ii, 25)
+#print res 
